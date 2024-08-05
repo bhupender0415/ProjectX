c027396cd58f97b5ee3ed5b068dff38cf962aa16
Bhupender Singh <bhupender0415@gmail.com>
2024-08-06 01:35:40 +0530

    implemented uart in interrupt mode

---
+++ flash.bat
@@ -0,0 +1,5 @@
+:: Deletes All files in the Current Directory With Prompts and Warnings 
+::(Hidden, System, and Read-Only Files are Not Affected) 
+
+openocd\bin\openocd.exe -f ./openocd.cfg -c "program Build/ProjectX.bin reset exit 0x08000000"
+
